046cbdaf06a20c1514a8423c6a894263c5bf0617
Roman Meier <rom-meier@hotmail.com>
2017-03-14 21:59:51 +0100

    Init

---
+++ Assets/Scripts/StateManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*
+ *Kontextklasse, die die States verwaltet.
+ */ 
+public class StateManager : MonoBehaviour {
+
+    private State momentaryState;
+
+
+    public void SetMomentaryState(State s)
+    {
+        momentaryState = s;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log(Input.mousePosition);
+        }
+
+
+    }
+}
+
+
+public interface State
+{
+
+
+}
---
+++ Assets/Scripts/States/MainMenuState.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuState : MonoBehaviour {
+
+
+    void OnGUI()
+    {
+
+        if (GUI.Button(new Rect(30, 30, 150, 30), "Start new Game"))
+        {
+            
+        }
+    }
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
